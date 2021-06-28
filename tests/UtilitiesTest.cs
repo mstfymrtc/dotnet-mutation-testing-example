082765bd1fee14d12ce32444701b64f31a690d81
Mustafa Yumurtacı <mustafa.yumurtaci@trendyol.com>
2021-06-29 02:20:26 +0300

    #add new test

--- tests/UtilitiesTest.cs
+++ tests/UtilitiesTest.cs
@@ -12,5 +12,13 @@ namespace Example.Tests
             var expected = 1;
             Assert.AreEqual(expected,actual);
         }
+            
+        [Test]
+        public void Sum_GivenOnePlusTwo_ShouldCalculateProperly()
+        {
+            var actual = Utilities.Sum(1,2);
+            var expected = 3;
+            Assert.AreEqual(expected,actual);
+        }
     }
 }
